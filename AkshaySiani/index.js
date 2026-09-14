d415edc96853bd11abac6fb6738509b709f3449a
Allah Rakha <allahrakhahsp1234@gmail.com>
2026-09-15 00:10:35 +0500

    feat: event loop and js code execution

--- AkshaySiani/index.js
+++ AkshaySiani/index.js
@@ -171,6 +171,7 @@
 //   console.log("Hello World")
 // }
 // greet();
+// console.log("hi")
 // ===> Function Expression
 // let greet1 = function () {
 //   console.log("Hello World 1")
@@ -216,3 +217,201 @@
 // }
 // x(greet)(); // Here x(greet) will return a function and then we are invoking that returned function with ().
 
+// ===> Factory Function ( a normal function that creates and returns an object )
+// function createCircle(radius) {
+//   return {
+//     radius: radius,
+//     draw: function () {
+//       console.log("Draw")
+//     },
+//     area: function () {
+//       console.log(`Area of circle with radius=${this.radius} is: ${Math.PI * this.radius * this.radius}`)
+//     }
+//   }
+// }
+// const circle1 = createCircle(1);
+// console.log("🚀 ~ circle1:", circle1)
+// circle1.area()
+// const circle2 = createCircle(2);
+// console.log("🚀 ~ circle2:", circle2)
+// circle2.area()
+
+// ===> Constructor Function
+// Uses `this` to define properties/methods on the object.
+// When called with `new`, JavaScript automatically:
+// 1. Creates a new empty object
+// 2. Sets `this` to that new object
+// 3. Links the object to Circle.prototype
+// 4. Executes the constructor function
+// 5. Returns the new object
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("Draw")
+//   };
+//   this.area = function () {
+//     console.log(`Area of circle with radius=${this.radius} is: ${Math.PI * this.radius * this.radius}`)
+//   };
+// };
+// const circle1 = new Circle(1);
+// console.log("🚀 ~ circle1:", circle1)
+// circle1.area()
+// const circle2 = new Circle(2);
+// console.log("🚀 ~ circle2:", circle2)
+// circle2.area()
+
+
+// ---------------------- Callback Functions, Event Listeners -----------------------
+
+// ===> Callback Functio in JS
+// function x(callback) {
+//   console.log("Starting x Function.")
+//   callback();
+// }
+// function y() {
+//   console.log("Starting y Function.")
+// }
+// x(y);
+
+// ===> JS is synchronous and single threaded. It means that it can execute one task at a time. But we can use callback functions to execute multiple tasks asynchronously. Callback functions are passed as arguments to other functions and are executed after the completion of the parent function.
+// setTimeout(function () {
+//   console.log("I am setTimeout function.");
+// }, 5000);
+// function x(callback) {
+//   console.log("Starting x Function.")
+//   callback();
+// }
+// function y() {
+//   console.log("Starting y Function.")
+// }
+// x(y);
+
+// ===> Blocking the MAIN thread means blocking the CALL STACK. e.g. In above example, if x func has a long running task, then y func will be executed after the completion of x func. But if we use setTimeout, then y func will be executed after the completion of x func and setTimeout func will be executed after the specified time.
+// setTimeout(function () {
+//   console.log("I am outer setTimeout function.");
+// }, 5000);
+// function x(callback) {
+//   console.log("Starting x Function.")
+//   setTimeout(function () {
+//     console.log("Functin X setTimeout");
+//   }, 5000);
+//   callback();
+// }
+// function y() {
+//   console.log("Starting y Function.")
+// }
+// x(y);
+
+// ===> Deep about Event Listeners in JS ( To make secure the "counter", we are making Closure)
+// function attchCounterWithEventListener() {
+//   var counter = 0;  // It will not removed by Garbage Collector becuase addEventListener holds its reference.
+//   var tempVar = 0; // It will removed by Garbage Collector
+//   document.getElementById("clickMe").addEventListener("click", function () {
+//     console.log("ClickMe clicked.", ++counter); // Stop here execution and access both variables in console and see
+//   })
+// }
+// attchCounterWithEventListener();
+
+// ===> Garbage Collector and Remove EventListeners ( Its important to remove eventListeners to freed the memory ) ( see in above example)
+// function attchCounterWithEventListener() {
+//   let counter = 0;  // It will not removed by Garbage Collector becuase addEventListener holds its reference.
+//   let tempVar = 0; // It will removed by Garbage Collector
+//   const button = document.getElementById("clickMe");
+//   const countHandler = function () {
+//     console.log("ClickMe clicked.", ++counter);
+//   }
+//   button.addEventListener("click", countHandler)
+//   button.removeEventListener("click", countHandler);
+//   console.log("Finished Listener Work");
+// }
+// attchCounterWithEventListener();
+
+
+// ---------------------- Asynchronous JavaScript & EVENT LOOP from scratch -----------------------
+
+// ===> CALL STACK
+// The Call Stack is where JavaScript code is executed.
+// Synchronous JavaScript runs here.
+// The Global Execution Context is initially pushed onto the Call Stack.
+
+
+// ===> WEB APIs
+// Browser-provided APIs that JavaScript can use.
+// Examples:
+//   - setTimeout
+//   - fetch
+//   - DOM APIs
+//   - event listeners
+//
+// These APIs are provided by the browser, not by the JavaScript engine itself.
+//
+// When we call an asynchronous Web API, the browser handles the
+// asynchronous operation outside the JavaScript Call Stack.
+//
+// Example:
+// setTimeout(() => {
+//   console.log("Timer finished");
+// }, 2000);
+//
+// The timer is handled by the browser.
+// After the timer finishes, its callback becomes eligible to be
+// placed into the appropriate task/callback queue.
+
+
+// ===> CALLBACK / TASK QUEUE
+// Also commonly called the Task Queue.
+//
+// Callbacks from APIs such as:
+//   - setTimeout
+//   - setInterval
+//   - DOM events
+//
+// are placed into the Task Queue when they are ready to execute.
+//
+// The Event Loop eventually moves them to the Call Stack
+// when the Call Stack is empty.
+
+
+// ===> MICROTASK QUEUE
+// Microtasks have higher priority than normal tasks.
+//
+// Examples:
+//   - Promise.then()
+//   - Promise.catch()
+//   - Promise.finally()
+//   - queueMicrotask()
+//   - Mutation Observer
+//
+// Important:
+// fetch() itself is NOT a microtask.
+//
+// fetch() returns a Promise.
+// When that Promise is settled, its .then() / .catch() / .finally()
+// callbacks are scheduled as microtasks.
+//
+// async/await also uses Promises.
+// The code after an await resumes through the Promise/microtask mechanism.
+
+
+// ===> EVENT LOOP
+// The Event Loop continuously checks whether JavaScript can execute
+// another piece of work.
+//
+// Simplified model:
+//
+// 1. Execute synchronous code from the Call Stack.
+// 2. When the Call Stack becomes empty, process Microtasks.
+// 3. Then the Event Loop can take a Task/Callback from the Task Queue
+//    and put it onto the Call Stack.
+// 4. Repeat.
+//
+// Microtasks generally get priority over normal tasks.
+
+// console.log("Start");
+// setTimeout(function () {
+//   console.log("I am setTimeout!")
+// }, 2000); // The callback becomes eligible after 2 seconds, but it can execute only when the call stack is empty. Also setTimeout means Don't execute this callback before 2 seconds. After 2 seconds, put it in a queue when the event loop can process it.
+// for (let i = 0; i < 10000; i++) {
+//   console.log("I: ", i)
+// };
+// console.log("End");
